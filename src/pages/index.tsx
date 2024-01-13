92ca265f2a99019203fd31bf92c3b2590c6415a1
HediehKh <113653779+HediehKh@users.noreply.github.com>
2024-01-12 19:37:15 -0800

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,10 @@ const DynamicMap = dynamic(() => import('../../components/Map'), {ssr:false});
 export default function Home() {
   return (
     <>
-      <div>
       <div className="topheader">
-      <h1>Hedieh Canada Line</h1>
-      <Image src="/images/Logo.png" 
-      width={20} height={20} alt="Logo"/>
+      <div className="justify-center">
+        <h1>Hedieh Canada Line</h1>
+      <Image src="/images/Logo.png" alt="Logo" width={20} height={20}/>
       </div>
       <DynamicMap/>
     </div>
